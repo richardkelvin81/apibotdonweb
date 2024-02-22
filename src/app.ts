9e246840a40f6e4d3ea0b9a79bbc133f20d4f536
richardkelvin81 <richard.aparicio@gmail.com>
2024-02-21 23:28:49 -0400

    firts commits

---
+++ src/app.ts
@@ -0,0 +1,28 @@
+
+import { createBot, createFlow, MemoryDB, createProvider, addKeyword } from "@bot-whatsapp/bot"
+import { BaileysProvider, handleCtx } from "@bot-whatsapp/provider-baileys"
+
+const flowBienvenida = addKeyword('hola').addAnswer ('Hola!, bienvenido a TAXICORP')
+
+const main = async  () =>{
+
+    const provider = createProvider (BaileysProvider)
+    provider.initHttpServer(3002)
+    provider.http?.server.post ('/enviar-whatsapp', handleCtx(async (bot,req,res)=>{
+        const phone = req.body.phone
+        const message = req.body.message
+        await bot.sendMessage(phone,message,{})
+        res.end ('Mensaje enviado desde servidor donWeb')
+    }))
+
+    await createBot ({
+
+        flow: createFlow([flowBienvenida]),
+        database: new MemoryDB(),
+        provider
+
+    })
+
+}
+
+main ()
